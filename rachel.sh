d614585dff9e7ea5daf00f9865cc559c35066346
Abigail McCarthy <abigail.mccarthy@noaa.gov>
2016-03-03 17:59:58 +0000

    second commit

--- rachel.sh
+++ rachel.sh
@@ -1,6 +1,7 @@
 #calculate reduced stats for data files at J: 100 c/bp
 for datafile in "$@"
 do	
-	echo $datafile
+	echo ${datafile}
 	bash goostats -J 100 -r $datafile stats-$datafile
+	#bob's your uncle
 done 
